7976c6d47cc90afa37a3243024b4008c62cba73a
DMcough1024 <90269348+DMcough1024@users.noreply.github.com>
2023-10-06 18:17:24 -0500

    Delete order_route.py
    Reuploading final versions.

--- order_route.py
+++
@@ -1,9 +0,0 @@
-def assign_drone(order, drones):
-    # check time to each drone, assign to the closest drone
-    x = 0
-    return drones[x]
-
-def closest_store(drone, stores):
-    # check time to each store, assign to the closest store
-    x = 0
-    return stores[x]
